04b00903567bda181edf0b8095a15b10f42f0b72
Furtif <msntchat@hotmail.fr>
2018-02-17 23:36:35 +0100

    verify token before save

--- PokemonGoGUI/Client.cs
+++ PokemonGoGUI/Client.cs
@@ -575,6 +575,8 @@ namespace PokemonGoGUI
 
         private void SaveAccessToken(AccessToken accessToken)
         {
+            if (accessToken == null || accessToken.Token == null || accessToken.IsExpired)
+                return;
             var fileName = Path.Combine(Directory.GetCurrentDirectory(), "Cache", $"{accessToken.Uid}.json");
             File.WriteAllText(fileName, JsonConvert.SerializeObject(accessToken, Formatting.Indented));
         }
